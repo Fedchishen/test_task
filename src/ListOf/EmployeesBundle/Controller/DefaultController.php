5bcd8ae50f9c328a10493045c2e74e53c9ff2363
Формула <@192.168.0.102>
2018-05-21 00:21:34 +0300

    Закончил #6 Добавьте возможность сортировать (и искать если Вы выполнили задачу No5) по ​любому ​полю ​без ​перезагрузки ​страницы, ​например ​используя ​ajax.

--- src/ListOf/EmployeesBundle/Controller/DefaultController.php
+++ src/ListOf/EmployeesBundle/Controller/DefaultController.php
@@ -12,34 +12,20 @@ class DefaultController extends Controller {
      */
     public function indexAction() {
         return $this->render('@ListOfEmployees/Default/index.html.twig', array(
-                    'employees' => $this->getList('ListOfEmployeesBundle:Employees', null, null), //Работники
+                    'employees' => $this->getDoctrine()->getRepository('ListOfEmployeesBundle:Employees')->findAll(), //Работники
                     'position' => $this->getPositions() //Должности
         ));
     }
 
-    //ВАЖНО!!! функции getList() и getPositions() дублируются в SortListController.php
-    //Надо решить вопрос с выносом этих функций в отдельный класс
-    //
-    // получение информации из бд
-    public function getList($rep, $order_by, $index) {
-        if (empty($order_by)) {
-            $order_by = "id";
-        }
-        if (empty($index)) {
-            $index = "ASC";
-        }
-
-        return $this->getDoctrine()->getRepository($rep)
-                        ->findBy(array(), array($order_by => $index));
-    }
-
     //получение массива с должностями, где $array[ранк_должности]=="Название должности"
     public function getPositions() {
-        $positions = $this->getList('ListOfEmployeesBundle:Positions', null, null);
+        $positions = $this->getDoctrine()->getRepository('ListOfEmployeesBundle:Positions')
+                ->findAll();
         $positions_new = array();
         for ($i = 0; $i < count($positions); $i++) {
             $positions_new[$positions[$i]->getPositionRank()] = $positions[$i]->getPositionName();
         }
         return $positions_new;
     }
+
 }
--- src/ListOf/EmployeesBundle/Controller/SortListController.php
+++ src/ListOf/EmployeesBundle/Controller/SortListController.php
@@ -6,81 +6,69 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Doctrine\ORM\Query\Expr;
 
 class SortListController extends Controller {
 
     /**
-     * @Route("/sort/{order_by}/{index}", name="sort")
-     */
-    public function sortAction($order_by = "id", $index = "ASC") {
-        return $this->render('@ListOfEmployees/Default/sortlist.html.twig', array(
-                    'employees' => $this->orderByList('ListOfEmployeesBundle:Employees', $order_by, $index), //Работники
-                    'position' => $this->getPositions() //Должности
-        ));
-    }
-
-    /**
      * @Route("/search", name="search")
      */
-    public function searchAction(Request $request) {
-            
+    public function searchSortAction(Request $request) {
+
         if ($request->isXmlHttpRequest()) {
-            
-            $name = $request->request->get('name');             
-            $like = $request->request->get('like');  
-
+            //Получаю массив должностей $array[ранк_должности]=="Название должности"
             $positions = $this->getPositions();
+
+            //Полученную инфу от Ajax функции раскидываю по переменным
+            $name = $request->request->get('name');
+            $like = $request->request->get('like');
+            $order_by = $request->request->get('orderby');
+            $index = $request->request->get('index');
+
             
-            $employees = $this->SearchLikeList('ListOfEmployeesBundle:Employees', $name, $like);
-                $jsonData = array();  
-                $idx = 0;  
-                foreach($employees as $em) {  
-                    $temp = array(
-                        'id' => $em->getId(),  
-                        'surname' => $em->getSurname(),
-                        'firstname' => $em->getFirstName(),
-                        'middlename' => $em->getMiddleName(),
-                        'positionid' => $positions[$em->getPositionId()],
-                        'dateofemployment' => $em->getDateOfEmployment()->format('Y-m-d'),
-                        'wages' => $em->getWages(),
-                        'chiefid' => $em->getChiefId()
-                    );   
-                    $jsonData[$idx++] = $temp;  
-                } 
-            return new JsonResponse($jsonData); 
+            // Проверяю на null
+            if (is_null($name) || is_null($like)) {
+                $name_like = array();
+            } else {
+                $name_like = array($name => $like);
+            }
             
-            //return $response;
+            // Проверяю на null
+            if (is_null($order_by) || is_null($index)) {
+                $order_by_index = array();
+            } else {
+                $order_by_index = array($order_by => $index);
+            }
             
+            //Получаю работников
+            $employees = $this->getDoctrine()->getRepository('ListOfEmployeesBundle:Employees')
+                    ->findBy($name_like, $order_by_index);
+
+            //Формирую json c работниками для ответа в ajax функцию 
+            $jsonData = array();
+            $idx = 0;
+            foreach ($employees as $em) {
+                $temp = array(
+                    'id' => $em->getId(),
+                    'surname' => $em->getSurname(),
+                    'firstname' => $em->getFirstName(),
+                    'middlename' => $em->getMiddleName(),
+                    'positionid' => $positions[$em->getPositionId()],
+                    'dateofemployment' => $em->getDateOfEmployment()->format('Y-m-d'),
+                    'wages' => $em->getWages(),
+                    'chiefid' => $em->getChiefId()
+                );
+                $jsonData[$idx++] = $temp;
+            }
+            return new JsonResponse($jsonData);
         } else {
-            return $this->render('@ListOfEmployees/Default/sortlist.html.twig', array(
-                        'employees' => $this->SearchLikeList('ListOfEmployeesBundle:Employees', "chief_id", "4"), //Работники
-                        'position' => $this->getPositions() //Должности
-            ));
+            return $this->render('@ListOfEmployees/Default/sortlist.html.twig');
         }
-    }
-
-    public function SearchLikeList($rep, $name, $like) {
-        return $this->getDoctrine()->getRepository($rep)
-                        ->findBy(array($name => $like));
-    }
-
-    // получение информации из бд
-    public function orderByList($rep, $order_by, $index) {
-        if (empty($order_by)) {
-            $order_by = "id";
-        }
-        if (empty($index)) {
-            $index = "ASC";
-        }
-
-        return $this->getDoctrine()->getRepository($rep)
-                        ->findBy(array(), array($order_by => $index));
     }
 
     //получение массива с должностями, где $array[ранк_должности]=="Название должности"
     public function getPositions() {
-        $positions = $this->orderByList('ListOfEmployeesBundle:Positions', null, null);
+        $positions = $this->getDoctrine()->getRepository('ListOfEmployeesBundle:Positions')
+                ->findAll();
         $positions_new = array();
         for ($i = 0; $i < count($positions); $i++) {
             $positions_new[$positions[$i]->getPositionRank()] = $positions[$i]->getPositionName();
