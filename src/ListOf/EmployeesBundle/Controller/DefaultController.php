6e66f7ef43b83c7679d01aa780e701da9aef4d3e
Формула <@192.168.0.102>
2018-05-23 13:33:33 +0300

    13. Реализуйте возможность менять начальника сотрудника используя drag-n-drop сразу ​в ​дереве ​сотрудников.

--- src/ListOf/EmployeesBundle/Controller/DefaultController.php
+++ src/ListOf/EmployeesBundle/Controller/DefaultController.php
@@ -4,8 +4,14 @@ namespace ListOf\EmployeesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use ListOf\EmployeesBundle\Controller\SortListController;
+
+
+
+
+
+
 
 class DefaultController extends Controller {
 
@@ -19,6 +25,32 @@ class DefaultController extends Controller {
         ));
     }
     
+     /**
+     * @Route("/newchief", name="newchief")
+     */
+    public function newchiefAction(Request $request) {
+        if ($request->isXmlHttpRequest()) {
+            $id = $request->request->get('id');
+            $chief_id = $request->request->get('chief_id');
+            
+                $entityManager = $this->getDoctrine()->getManager();
+                $em = $entityManager->getRepository('ListOfEmployeesBundle:Employees')->find($id);
+
+                if (!$em) {
+                    throw $this->createNotFoundException(
+                        'No employees found for id '.$id
+                    );
+                }
+
+                $em->setChiefId($chief_id);
+                $entityManager->flush();
+            
+            return new Response('YES', Response::HTTP_OK);
+        }
+        else{return new Response('NO', Response::HTTP_OK);}
+
+    }
+//    
 //     /**
 //     * @Route("/byid" name="byid")
 //     */
